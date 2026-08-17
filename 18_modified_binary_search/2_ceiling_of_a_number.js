bbf4f1ed02353a6f815a4eaa65d20f2dca429c0b
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-17 20:26:27 +0400

    ceiling of a number

---
+++ 18_modified_binary_search/2_ceiling_of_a_number.js
@@ -0,0 +1,12 @@
+// Ceiling of a Number: Given an array of numbers sorted in an ascending order, find the ceiling of a given number ‘key’. 
+// The ceiling of the ‘key’ will be the smallest element in the given array greater than or equal to the ‘key’.
+
+// Write a function to return the index of the ceiling of the ‘key’. If there isn’t any ceiling return -1.
+
+// Example 1:
+
+// Input: [4, 6, 10], key = 6
+// Output: 1
+
+
+// solution:
