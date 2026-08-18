458d9d1c2f3ff310186e875403bc0532771f167e
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-18 19:47:12 +0400

    next letter

---
+++ 18_modified_binary_search/3_next_letter.js
@@ -0,0 +1,12 @@
+// Next Letter: Given an array of lowercase letters sorted in ascending order, find the smallest letter in the given array greater than a given ‘key’.
+// Assume the given array is a circular list, which means that the last letter is assumed to be connected with the first letter. This also means that 
+// the smallest letter in the given array is greater than the last letter of the array and is also the first letter of the array.
+// Write a function to return the next letter of the given ‘key’.
+
+// Example 1:
+
+// Input: ['a', 'c', 'f', 'h'], key = 'f'
+// Output: 'h'
+
+
+// solution:
