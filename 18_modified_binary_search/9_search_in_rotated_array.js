827e20bedb913aec7d195a65401b5792938ed48b
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-20 19:05:31 +0400

    search in rotated array

---
+++ 18_modified_binary_search/9_search_in_rotated_array.js
@@ -0,0 +1,14 @@
+// Search in Rotated Array: Given an array of numbers which is sorted in ascending order and also rotated 
+// by some arbitrary number, find if a given ‘key’ is present in it.
+
+// Write a function to return the index of the ‘key’ in the rotated array. If the ‘key’ is not present, 
+// return -1. You can assume that the given array does not have any duplicates.
+
+// Note: You need to solve the problem in  time complexity.
+
+// Example 1:
+
+// Input: [10, 15, 1, 3, 8], key = 15
+// Output: 1
+
+// solution:
