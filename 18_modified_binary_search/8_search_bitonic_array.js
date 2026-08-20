719eb479fa0481e7fea3cb0278d1a47ebe75e82c
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-20 18:33:27 +0400

    search bitonic array

---
+++ 18_modified_binary_search/8_search_bitonic_array.js
@@ -0,0 +1,9 @@
+// Search Bitonic Array: Given a Bitonic array, find if a given ‘key’ is present in it. An array is considered bitonic if it is first 
+// monotonically increasing and then monotonically decreasing.
+
+// In other words, a bitonic array starts with a sequence of increasing elements, reaches a peak element, and then follows with a 
+// sequence of decreasing elements. The peak element is the maximum value in the array.
+
+// Write a function to return the index of the ‘key’. If the 'key' appears more than once, return the smaller index. If the ‘key’ is not present, return -1.
+
+// solution:
