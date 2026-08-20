327e7c13666a1f11e01d6625e6f58bf8fc0104e4
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-20 18:35:02 +0400

    search bitonic array solution theory

--- 18_modified_binary_search/8_search_bitonic_array.js
+++ 18_modified_binary_search/8_search_bitonic_array.js
@@ -7,3 +7,35 @@
 // Write a function to return the index of the ‘key’. If the 'key' appears more than once, return the smaller index. If the ‘key’ is not present, return -1.
 
 // solution:
+// This problem is basically 3 steps:
+
+// 1. Find the peak
+// 2. Binary search the increasing part
+// 3. If not found, binary search the decreasing part
+
+// The important detail is:
+
+// Search the increasing part first, because if the key appears on both sides, the left side has the smaller index.
+
+// Example
+// [1, 3, 8, 12, 8, 5, 2]
+//           ↑
+//         peak
+
+// Suppose:
+
+// key = 8
+
+// There are two 8s:
+
+// index:  0  1  2   3  4  5  6
+//         1  3  8  12  8  5  2
+//            ↑          ↑
+//           2           4
+
+// We must return:
+
+// 2
+
+// not 4.
+
