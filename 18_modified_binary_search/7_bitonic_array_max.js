76a406322dd0d9debaca1a2b135b7be840f51838
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-20 18:14:35 +0400

    bitonic array max

---
+++ 18_modified_binary_search/7_bitonic_array_max.js
@@ -0,0 +1,11 @@
+// Bitonic Array Maximum: Find the maximum value in a given Bitonic array. An array is considered bitonic 
+// if it is first monotonically increasing and then monotonically decreasing.
+
+// In other words, a bitonic array starts with a sequence of increasing elements, reaches a peak element, 
+// and then follows with a sequence of decreasing elements. The peak element is the maximum value in the array.
+
+// Example 1:
+
+// Input: [1, 3, 8, 12, 4, 2]
+// Output: 12
+
