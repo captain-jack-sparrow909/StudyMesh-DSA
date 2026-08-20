8f015d40ca2ff0458a9ef34792899e1e0899683a
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-20 20:36:06 +0400

    rotation count

---
+++ 18_modified_binary_search/10_rotation_count.js
@@ -0,0 +1,19 @@
+// Rotation Count: Given an array of numbers which is sorted in ascending order and is rotated ‘k’ times around a pivot, find ‘k’.
+
+// You can assume that the array does not have any duplicates.
+
+// Note: You need to solve the problem in  time complexity.
+
+// Example 1:
+
+// Input: [10, 15, 1, 3, 8]
+// Output: 2
+// Explanation: The array has been rotated 2 times.
+// Example 2:
+
+// Input: [4, 5, 7, 9, 10, -1, 2]
+// Output: 5
+// Explanation: The array has been rotated 5 times.
+
+
+// solution:
