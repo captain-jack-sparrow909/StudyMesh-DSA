5cdd3f7fb2fb45d48cdf3d1816f6d15cce2538d1
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-21 18:38:32 +0400

    single number

---
+++ 19_Bitwise_XOR/1_single_number.js
@@ -0,0 +1,8 @@
+// Single Number: In a non-empty array of integers, every number appears twice except for one, find that single number.
+
+// Example 1:
+
+// Input: 1, 4, 2, 1, 3, 2, 3
+// Output: 4
+
+// solution:
