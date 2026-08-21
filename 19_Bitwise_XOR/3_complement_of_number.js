f16b48dc3d14e6e12f1907ce1a32f0a6db1b1a63
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-21 20:43:15 +0400

    bitwise complement code explained

--- 19_Bitwise_XOR/3_complement_of_number.js
+++ 19_Bitwise_XOR/3_complement_of_number.js
@@ -118,3 +118,112 @@ function bitwiseComplement(num) {
     return num ^ mask;
 }
 
+
+
+// Let's understand these two lines
+// mask = (mask << 1) | 1;
+
+// Suppose:
+
+// mask = 111
+
+// mask << 1 shifts everything left:
+
+// 1110
+
+// Then:
+
+// 1110 | 0001
+
+// gives:
+
+// 1111
+
+// So we're basically doing:
+
+// 1
+// 11
+// 111
+// 1111
+
+// And:
+
+// n = n >> 1;
+
+// moves the bits of n one position to the right.
+
+// For 8:
+
+// 1000
+//  ↓
+// 0100
+//  ↓
+// 0010
+//  ↓
+// 0001
+//  ↓
+// 0000
+
+// This lets us count how many bits num has.
+
+// Full example: 8
+
+// Initially:
+
+// n = 8       → 1000
+// mask = 0
+// Loop 1
+// mask = 0 << 1 | 1
+//      = 1
+// n = 1000 >> 1
+//   = 0100
+// Loop 2
+// mask = 1 << 1 | 1
+//      = 11
+// n = 0100 >> 1
+//   = 0010
+// Loop 3
+// mask = 11 << 1 | 1
+//      = 111
+// n = 0010 >> 1
+//   = 0001
+// Loop 4
+// mask = 111 << 1 | 1
+//      = 1111
+// n = 0001 >> 1
+//   = 0000
+
+// Loop stops.
+
+// Now:
+
+// num  = 1000
+// mask = 1111
+
+// XOR:
+
+//   1000
+// ^ 1111
+// ------
+//   0111
+
+// 0111 = 7.
+
+// The main idea to remember
+// Number:
+// 1000
+
+// Create mask:
+// 1111
+
+// XOR:
+// 1000
+// ^1111
+// =0111
+
+// So:
+
+// Create a mask of all 1s with the same number of bits as N, then XOR N with the mask.
+
+// Time complexity: O(log N)
+// Space complexity: O(1).
