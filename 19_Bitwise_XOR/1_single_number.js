0ad002932da4612e924af7d34fb6b2bd6af7449b
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-21 18:39:43 +0400

    single number solution theory

--- 19_Bitwise_XOR/1_single_number.js
+++ 19_Bitwise_XOR/1_single_number.js
@@ -6,3 +6,36 @@
 // Output: 4
 
 // solution:
+// This one is very simple once you know XOR (^).
+
+// The trick is:
+
+// Every number appears twice, so the pairs can cancel each other out.
+
+// XOR rules
+
+// You only need to remember 2 rules:
+
+// number ^ same number = 0
+// number ^ 0 = number
+
+// For example:
+
+// 4 ^ 4 = 0
+// 7 ^ 0 = 7
+
+// So if we XOR everything:
+
+// 1 ^ 4 ^ 2 ^ 1 ^ 3 ^ 2 ^ 3
+
+// The pairs cancel:
+
+// (1 ^ 1) ^ (2 ^ 2) ^ (3 ^ 3) ^ 4
+
+// Becomes:
+
+// 0 ^ 0 ^ 0 ^ 4
+
+// And finally:
+
+// 4
