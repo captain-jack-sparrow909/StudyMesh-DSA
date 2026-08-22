e1673b419043d641e27cf1d25e6818fd53d853f4
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-22 20:06:18 +0400

    kth smallest number and renaming

---
+++ 20_Top_K_elements/2_Kth_smallest_number.js
@@ -0,0 +1,5 @@
+// Kth Smallest Number: Given an unsorted array of numbers, find Kth smallest number in it.
+
+// Please note that it is the Kth smallest number in the sorted order, not the Kth distinct element.
+// Input: [1, 5, 12, 2, 11, 5], K = 3 Output: 5
+
