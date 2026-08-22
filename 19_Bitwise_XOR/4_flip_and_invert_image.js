81415fc9cc2c1ec279c20c084eba0bfa463914b1
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-22 15:37:47 +0400

    flip and invert an image code

--- 19_Bitwise_XOR/4_flip_and_invert_image.js
+++ 19_Bitwise_XOR/4_flip_and_invert_image.js
@@ -49,3 +49,15 @@
 
 // [0, 1, 0]
 
+function flipAndInvertImage(image) {
+    for (let row of image) {
+      row.reverse();
+  
+      for (let i = 0; i < row.length; i++) {
+        row[i] = row[i] === 0 ? 1 : 0;
+      }
+    }
+  
+    return image;
+}
+
