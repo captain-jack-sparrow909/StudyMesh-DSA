ea6f828537e04088b742fd4c9a01c5880029707d
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-23 13:52:26 +0400

    connect ropes

---
+++ 20_Top_K_elements/4_connect_ropes.js
@@ -0,0 +1,20 @@
+// Connect Ropes: Given ‘N’ ropes with different lengths, we need to connect these ropes into one big rope with minimum cost. 
+// The cost of connecting two ropes is equal to the sum of their lengths.
+
+// **Example 1**:
+
+// ```
+// Input: [1, 3, 11, 5]
+// Output: 33
+// Explanation: First connect 1+3(=4), then 4+5(=9), and then 9+11(=20). So the total cost is 33 (4+9+20)
+
+// ```
+
+// **Example 2**:
+
+// ```
+// Input: [3, 4, 5, 6]
+// Output: 36
+// Explanation: First connect 3+4(=7), then 5+6(=11), 7+11(=18). Total cost is 36 (7+11+18)
+// ```
+
