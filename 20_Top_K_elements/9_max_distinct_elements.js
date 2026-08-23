d720fbd93f331c2c5f7d8eb59a2f620fb8d88cc2
Jabir Khan <98869091+captain-jack-sparrow909@users.noreply.github.com>
2026-08-23 20:24:11 +0400

    max distinct elements

---
+++ 20_Top_K_elements/9_max_distinct_elements.js
@@ -0,0 +1,10 @@
+// Maximum Distinct Elements: Given an array of numbers `nums` and an integer `K`, 
+// find the maximum number of `distinct` elements after removing exactly `K` elements from the `nums` array.
+
+// **Example 1**:
+
+// - **Input:** nums = [7, 3, 5, 8, 5, 3, 3], K=2
+// - **Expected Output:** 4
+
+
+// solution:
